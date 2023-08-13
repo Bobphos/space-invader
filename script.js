b9161dbd70d32d4e15a2214df3869d0040119439
Bobphos <zegameplay52@gmail.com>
2023-08-13 17:38:58 -0400

    Co-authored-by: Patrick Delaney <patdel0@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const square = document.createElement("div");
-square.classList.add("square");
-document.body.appendChild(square);
+const canvas = document.getElementById("ca");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+let x = 0;
+let y = 20;
+const width = 100;
+const height = 150;
+
+// setInterval(drawSquare, 10)
+
+function drawSquare() {
+    resetCanvas()
+    ctx.fillRect(x, y, width, height);
+}
+drawSquare()
+
+function resetCanvas(){
+ ctx.clearRect(0,0, canvas.width, canvas.height);
+}
+
+document.addEventListener('keydown', (event) => {
+//   console.log(`key pressed: ${event.key}`);
+    if (event.key === 'w' || event.key === 'W'){
+        // Mudar a coordenada, aumentar ou diminuir x
+        x++;
+        // Desenhar o quadrado
+        ctx.fillRect(x, y, width, height);
+    }
+});
+
+
+
